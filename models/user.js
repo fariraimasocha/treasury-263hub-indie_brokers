c3010c0b403032700ab9b7c0369529d0f9273647
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-04-12 18:02:52 +0200

    Add user model with schema definition and refresh model utility

---
+++ models/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { refreshModel } from "@/utils/modelUtils";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = refreshModel("User", userSchema);
+
+export default User;
