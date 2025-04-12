9b9b5c3a6c6376733c444289441e7ef7d23b0e7a
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-04-12 18:33:00 +0200

    Add layout and providers for query and session management

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Treasury Landing Page</div>;
+  return (
+    <div>
+      Treasury Landing Page
+      <Link href="/login">
+        <Button variant="outline">Register</Button>
+      </Link>
+    </div>
+  );
 }
